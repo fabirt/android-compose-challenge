5e6a81c8c53f34843e5bdd366d11b4d29b2ce6e7
Fabirt <fandig27@gmail.com>
2021-03-15 16:22:25 -0500

    Custom Text Selection Colors

--- Bloom/app/src/main/java/com/fabirt/bloom/ui/login/LoginTextField.kt
+++ Bloom/app/src/main/java/com/fabirt/bloom/ui/login/LoginTextField.kt
@@ -1,15 +1,13 @@
 package com.fabirt.bloom.ui.login
 
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.VisualTransformation
 
@@ -24,29 +22,48 @@ fun LoginTextField(
     onValueChanged: (String) -> Unit
 ) {
     var value by rememberSaveable { mutableStateOf("") }
-    SelectionContainer {
-        OutlinedTextField(
-            value = value,
-            placeholder = { Text(text = hintText) },
-            label = { Text(text = labelText) },
-            onValueChange = {
-                value = it
-                onValueChanged(it)
-            },
-            keyboardOptions = keyboardOptions,
-            visualTransformation = visualTransformation,
-            textStyle = MaterialTheme.typography.body1.copy(
-                color = MaterialTheme.colors.onBackground
-            ),
-            keyboardActions = keyboardActions,
-            modifier = modifier,
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                focusedBorderColor = MaterialTheme.colors.onBackground,
-                unfocusedBorderColor = MaterialTheme.colors.onBackground,
-                cursorColor = MaterialTheme.colors.onBackground,
-                focusedLabelColor = MaterialTheme.colors.onBackground,
-                unfocusedLabelColor = MaterialTheme.colors.onBackground
+    val color = MaterialTheme.colors.onBackground
+
+    // Modifies text selection colors (by default it will take the primary color from MaterialTheme)
+    val selectionColors = rememberTextSelectionColors(MaterialTheme.colors.copy(primary = color))
+
+    CompositionLocalProvider(LocalTextSelectionColors provides selectionColors) {
+        SelectionContainer {
+            OutlinedTextField(
+                value = value,
+                placeholder = { Text(text = hintText) },
+                label = { Text(text = labelText) },
+                onValueChange = {
+                    value = it
+                    onValueChanged(it)
+                },
+                keyboardOptions = keyboardOptions,
+                visualTransformation = visualTransformation,
+                textStyle = MaterialTheme.typography.body1.copy(
+                    color = color
+                ),
+                keyboardActions = keyboardActions,
+                modifier = modifier,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = color,
+                    unfocusedBorderColor = color,
+                    cursorColor = color,
+                    focusedLabelColor = color,
+                    unfocusedLabelColor = color
+                )
             )
+        }
+    }
+}
+
+@Composable
+internal fun rememberTextSelectionColors(colors: Colors): TextSelectionColors {
+    val primaryColor = colors.primary
+
+    return remember(primaryColor) {
+        TextSelectionColors(
+            handleColor = primaryColor,
+            backgroundColor = primaryColor.copy(alpha = 0.16f),
         )
     }
 }
