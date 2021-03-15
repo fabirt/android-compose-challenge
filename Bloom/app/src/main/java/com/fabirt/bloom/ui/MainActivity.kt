67742406b6277445a422c643d18226079477a00d
Fabirt <fandig27@gmail.com>
2021-03-15 06:13:14 -0500

    Creating launcher icon

--- Bloom/app/src/main/java/com/fabirt/bloom/ui/MainActivity.kt
+++ Bloom/app/src/main/java/com/fabirt/bloom/ui/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.WindowCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.fabirt.bloom.R
 import com.fabirt.bloom.navigation.Destinations
 import com.fabirt.bloom.ui.home.HomeScreen
 import com.fabirt.bloom.ui.login.LoginScreen
@@ -20,6 +21,7 @@ class MainActivity : ComponentActivity() {
     @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Bloom)
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
